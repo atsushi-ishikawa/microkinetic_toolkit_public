c4286040ab4dadbdd221fba46f006198665190bd
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2018-01-10 13:53:56 +0900

    conflict resolved

--- surf.py
+++ surf.py
@@ -1,8 +1,16 @@
-from ase.build import fcc111
+from ase.build import fcc111, surface
 from ase.calculators.emt import EMT
 from ase.db import connect
+from ase.io import read
 
-surf    =  fcc111(symbol="Cu", size=[3,3,4], a=3.6, vacuum=10.0)
+vacuum = 10.0
+
+surf =  fcc111(symbol="Cu", size=[3,3,4], a=3.6, vacuum=vacuum)
+#bulk = read("mgo2.cif")
+#surf = surface(lattice=bulk, indices=(1,0,0), layers=3, vacuum=vacuum)
+#surf = surf*[3,3,1]
+
+surf.translate([0,0,-vacuum])
 
 lattice = "fcc"
 facet   = "111"
