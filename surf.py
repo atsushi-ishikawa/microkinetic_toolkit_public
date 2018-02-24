3b60d7c6473e71ceee64357c07258a74e0a7170c
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2018-02-24 10:32:17 +0900

    asdf

--- surf.py
+++ surf.py
@@ -5,15 +5,15 @@ from ase.io import read
 
 vacuum = 10.0
 
-surf =  fcc111(symbol="Cu", size=[3,3,4], a=3.6, vacuum=vacuum)
-#bulk = read("mgo2.cif")
-#surf = surface(lattice=bulk, indices=(1,0,0), layers=3, vacuum=vacuum)
-#surf = surf*[3,3,1]
+#surf =  fcc111(symbol="Cu", size=[3,3,4], a=3.6, vacuum=vacuum)
+bulk = read("mgo.cif")
+surf = surface(lattice=bulk, indices=(1,0,0), layers=3, vacuum=vacuum)
+surf = surf*[3,3,1]
 
 surf.translate([0,0,-vacuum])
 
 lattice = "fcc"
-facet   = "111"
+facet   = "100"
 
 pos = {	'lattice' : lattice, 
 		'facet'   : facet }
