cf3f09cd0a36f423b11a98a6321915a3569b23fc
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2018-07-06 20:55:03 +0900

    from mac

--- make_rate_equation.py
+++ make_rate_equation.py
@@ -7,41 +7,45 @@ argvs = sys.argv
 reactionfile = argvs[1]
 
 (r_ads, r_site, r_coef,  p_ads, p_site, p_coef) = get_reac_and_prod(reactionfile)
+
+# r_ads and p_ads are species list of ALL the elementary reactions.
+# e.g. if inputfile contains
+#  (1) A1 + B1 --> C1 + D1
+#  (2) A2 + B2 --> C2 + D2
+# it gives 
+# r_ads = [ [['A1'],['B1']] , [['A2'],['B2']] ]
+# p_ads = [ [['C1'],['D1']] , [['C2'],['D2']] ]
+
 outputfile = "met001ode.m"
 fout = open(outputfile,"w")
 
-fout.write('function dydt = ' + outputfile.replace('.m','') + '(~,y,yin,tau,A,Ea,deltaH,Wcat,Vr)')
+fout.write('function dydt = ' + outputfile.replace('.m','') + '(~,y,yin,tau,A,Ea,Kci,Wcat,Vr)')
 fout.write("\n\n")
 #
 # template - start
 #
 template = " \
-\t global T R Pin Ngas Ncomp species; \n \
+\t global T R Ngas Ncomp species Ncstr; \n \
 \t \n \
-\t R = 8.314; \n \
+\t R  = 8.314; \n \
 \t NA = 6.02*10^23; % Avogadro's number \n \
 \n \
-\t Afor = A(:,1); Arev = A(:,2); \n \
+\t Afor = A(:,1); \n \
 \n \
 \t kfor = Afor .* exp(-Ea/R/T); \n \
-\t krev = Arev .* exp(-(Ea-deltaH)/R/T); \n \
+\t krev = kfor ./ (Kci * 10^6); \n \
 \n \
-\t F = y(1:Ngas);\n  \
-\t F_tot = sum(F);\n \
-\t x = F/F_tot;\n \
-\t P = Pin*x;\n \
+\t C = y(1:Ngas);\n  \
 \t theta = y(1:Ncomp);\n \
 \t theta(1:Ngas) = 0;\n \
 \n \
-\t f_act = 1.0*10^-2; % fraction of active site\n \
-\t MW    = 101.1; % atomic mass of Ru [g/mol]\n \
-\t Natom = (Wcat/MW)*NA; % number of atoms in one g-cat [atoms/g]\n \
-\t Nsurf = Natom^(2/3); % number of atoms in surface\n \
-\t Nsite = Nsurf*f_act; % number of active sites\n \
+\t area  = 100 * Wcat;  % [cm^2] = [cm^2/g] * [g] \n \
+\t % area  = 1000/Ncstr;  % specify area directly (in cm^2) \n \
+\t % Nsite = Nsurf*f_act; % number of active sites\n \
+\t sden = 2.7*10^-9;   % site density [mol/cm^2]\n \
+\t theta = theta * sden; \n \
 \n \
-\t Fin = yin(1:Ngas);\n \
-\t theta = theta * Nsite;\n \
-\t % theta = theta * (Wcat/Vr);\n"
+\t Cin = yin(1:Ngas);\n"
 
 fout.write(template)
 #
@@ -57,10 +61,11 @@ dict2 = {}
 for irxn in range(rxn_num):
 	rxn_idx = str(irxn + 1) # MATLAB-index starts from 1
 
+	# hash-tag based reactant and product species list FOR THIS REACTION
 	list_r = []
 	list_p = []
 
-	# prepare dict1 -- start
+	# making dict1, a dictionary of species with number hash
 	for imol,mol in enumerate(r_ads[irxn]):
 		mol = mol[0]
 		site = r_site[irxn][imol][0]
@@ -78,7 +83,7 @@ for irxn in range(rxn_num):
 		spe = get_species_num(mol) + 1 # MATLAB
 		list_p.append(spe)
 		dict1[spe] = mol
-	# prepare dict1 -- end
+	# making dict1 --- done
 
 	#
 	# forward direction
@@ -89,53 +94,60 @@ for irxn in range(rxn_num):
 		site = r_site[irxn][imol][0]
 		if site !='gas':
 			mol = mol + "_surf"
-		spe  = get_species_num(mol) + 1 # MATLAB
+		spe = get_species_num(mol) + 1 # MATLAB
 
 		if site != 'gas' or mol  == 'surf':
 			theta = "theta(" + str(spe) + ")"
 		else:
-			theta = "P(" + str(spe) + ")"
+			theta = "C(" + str(spe) + ")"
 
 		power = r_coef[irxn][imol]
-		coefA = r_coef[irxn][imol]
 
 		if power != 1:
 			theta = theta + "^" + str(power)
 		tmp = tmp + "*" + theta
 
-	for mem in list_r:
-		#
-		# seeking for the coefficient 
-		#
-		for imol, mol in enumerate(r_ads[irxn]):
-			mol = mol[0]
-			if mol == dict1[mem]:
-				coefB = r_coef[irxn][imol]
+  	for mem in list_r:
+ 		coef = 0
+  		for imol, mol in enumerate(r_ads[irxn]):
+ 			mol = mol[0]
+			adsorbate = dict1[mem].split("_")[0]
+  			if mol == adsorbate:
+  				coef = r_coef[irxn][imol]
+		
+ 		if coef == 0:
+ 			print "something wrong at coef 1"; exit()
 
-		sto_coef = float(coefB)/float(coefA)
-		sto_coef = str(sto_coef)
-
+ 		sto_coef = str(float(coef))
 		if mem in dict2:
-			dict2[mem] = dict2[mem] + " - " + sto_coef + "*" + tmp
+			dict2[mem] = dict2[mem] + " -" + sto_coef + "*" + tmp
 		else:
-			dict2[mem] = " - " + sto_coef + "*" + tmp
+			dict2[mem] = " -" + sto_coef + "*" + tmp
 
-	for mem in list_p:
-		#
-		# seeking for the coefficient 
-		#
-		for imol, mol in enumerate(p_ads[irxn]):
-			mol = mol[0]
-			if mol == dict1[mem]:
-				coefB = p_coef[irxn][imol]
+		# multiply area/Vr for the surface reaction contribution to gas-phase species
+		if not "surf" in dict1[mem] and "theta" in dict2[mem]:
+			dict2[mem] = dict2[mem] + "*" + "(area/Vr)"
 
-		sto_coef = float(coefB)/float(coefA)
-		sto_coef = str(sto_coef)
+  	for mem in list_p:
+ 		coef = 0
+  		for imol, mol in enumerate(p_ads[irxn]):
+ 			mol = mol[0]
+			adsorbate = dict1[mem].split("_")[0]
+  			if mol == adsorbate:
+  				coef = p_coef[irxn][imol]
+		
+ 		if coef == 0:
+ 			print "something wrong at coef 2"; exit()
 
+ 		sto_coef = str(float(coef))
 		if mem in dict2:
 			dict2[mem] = dict2[mem] + " + " + sto_coef + "*" + tmp
 		else:
-			dict2[mem] = sto_coef + "*" + tmp
+			dict2[mem] = "  " + sto_coef + "*" + tmp
+
+		# multiply area/Vr for the surface reaction contribution to gas-phase species
+		if not "surf" in dict1[mem] and "theta" in dict2[mem]:
+			dict2[mem] = dict2[mem] + "*" + "(area/Vr)"
 
 	#
 	# reverse direction
@@ -151,49 +163,56 @@ for irxn in range(rxn_num):
 		if site != 'gas' or mol  == 'surf':
 			theta = "theta(" + str(spe) + ")"
 		else:
-			theta = "P(" + str(spe) + ")"
+			theta = "C(" + str(spe) + ")"
 
 		power = p_coef[irxn][imol]
-		coefA = p_coef[irxn][imol]
 
 		if power != 1:
 			theta = theta + "^" + str(power)
 		tmp = tmp + "*" + theta
 
-	for mem in list_r:
-		#
-		# seeking for the coefficient 
-		#
-		for imol, mol in enumerate(r_ads[irxn]):
-			mol = mol[0]
-			if mol == dict1[mem]:
-				coefB = r_coef[irxn][imol]
+  	for mem in list_r:
+ 		coef = 0
+  		for imol, mol in enumerate(r_ads[irxn]):
+ 			mol = mol[0]
+			adsorbate = dict1[mem].split("_")[0]
+  			if mol == adsorbate:
+  				coef = r_coef[irxn][imol]
+		
+ 		if coef == 0:
+ 			print "something wrong at coef 3"; exit()
 
-		sto_coef = float(coefB)/float(coefA)
-		sto_coef = str(sto_coef)
-
+ 		sto_coef = str(float(coef))
 		if mem in dict2:
 			dict2[mem] = dict2[mem] + " + " + sto_coef + "*" + tmp
 		else:
-			dict2[mem] = sto_coef + "*" + tmp
+			dict2[mem] = "  " + sto_coef + "*" + tmp
 
-	for mem in list_p:
-		#
-		# seeking for the coefficient 
-		#
-		for imol, mol in enumerate(p_ads[irxn]):
-			mol = mol[0]
-			if mol == dict1[mem]:
-				coefB = p_coef[irxn][imol]
+		# multiply area/Vr for the surface reaction contribution to gas-phase species
+		if not "surf" in dict1[mem] and "theta" in dict2[mem]:
+			dict2[mem] = dict2[mem] + "*" + "(area/Vr)"
 
-		sto_coef = float(coefB)/float(coefA)
-		sto_coef = str(sto_coef)
+  	for mem in list_p:
+ 		coef = 0
+  		for imol, mol in enumerate(p_ads[irxn]):
+ 			mol = mol[0]
+			adsorbate = dict1[mem].split("_")[0]
+  			if mol == adsorbate:
+  				coef = p_coef[irxn][imol]
+		
+ 		if coef == 0:
+ 			print "something wrong at coef 4"; exit()
 
+ 		sto_coef = str(float(coef))
 		if mem in dict2:
-			dict2[mem] = dict2[mem] + " - " + sto_coef + "*" + tmp
+			dict2[mem] = dict2[mem] + " -" + sto_coef + "*" + tmp
 		else:
-			dict2[mem] = " - " + sto_coef + "*" + tmp
+			dict2[mem] = " -" + sto_coef + "*" + tmp
 
+		# multiply area/Vr for the surface reaction contribution to gas-phase species
+		if not "surf" in dict1[mem] and "theta" in dict2[mem]:
+			dict2[mem] = dict2[mem] + "*" + "(area/Vr)"
+ 
 #
 # end loop for reaction
 #
@@ -217,21 +236,16 @@ fout.write(comment)
 # tempelate - start
 #
 template = "\
-\t %Rate = Rate/NA * Vr;\n \
-\t Rate = Rate/NA;\n \
 \t Rate(1:Ngas) = Rate(1:Ngas)*Vr;\n \
-\t Rate(Ngas+1:Ncomp) = Rate(Ngas+1:Ncomp)*10^0;\n \
-\t %Rate(1:Ngas)       = Rate(1:Ngas)*(1/NA)*Vr; % [molecule/site/s] --> [mol/s]\n \
-\t %Rate(Ngas+1:Ncomp) = Rate(Ngas+1:Ncomp)*(Nsite/NA)*Wcat; % [molecule/site/s] --> [mol/s]\n \
-\t %Rate(Ngas+1:Ncomp) = Rate(Ngas+1:Ncomp)*rho_b*v0; % [mol/s] --> [mol/s]*[g/m^3]*[m^3/s]=[mol/s^2]\n \
+\t Rate(Ngas+1:Ncomp) = Rate(Ngas+1:Ncomp)*(area/Vr);\n \
 \n \
 \t %fprintf('------------\\n');\n \
 \t %fprintf('%+12.8e %+12.8e %+12.8e %+12.8e %+12.8e \\n', Rate(1),Rate(2),Rate(3),Rate(4),Rate(5));\n \
-\t %fprintf('%+12.8e %+12.8e %+12.8e %+12.8e %+12.8e \\n', P(1),P(2),P(3),theta(4),theta(5));\n \
+\t %fprintf('%+12.8e %+12.8e %+12.8e %+12.8e %+12.8e \\n', C(1),C(2),C(3),theta(4),theta(5));\n \
 \n \
-\t dydt = 1/tau*(F - Fin) + Rate(1:Ngas); % gas\n \
+\t dydt = 1/tau*(Cin - C) + Rate(1:Ngas); % gas\n \
 \t if Ncomp > Ngas+1\n \
-\t\t dydt(Ngas+1:Ncomp) = Rate(Ngas+1:Ncomp); % species\n \
+\t\t dydt(Ngas+1:Ncomp) = Rate(Ngas+1:Ncomp)*(1/sden)*(Vr/area); % species\n \
 \t end \n \
 "
 #
