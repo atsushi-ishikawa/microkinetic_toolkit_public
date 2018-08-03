ff84a1a42461c6bc2d2d405f48b51bbc50b3a987
ishi <ishikawasuteado@gmail.com>
2018-08-03 15:01:20 +0900

    from whisky

--- add_volume_and_entropy.py
+++ add_volume_and_entropy.py
@@ -3,13 +3,18 @@ import os,sys
 from ase import Atoms, Atom
 from ase.calculators.gaussian import Gaussian
 from ase.db import connect
+from ase.optimize import BFGS
 #
 # calculate reaction energy
 # molecule's data should be stored in "methane.json"
 #
-# mol = sys.argv[1]
 calculator = "gau" ; calculator = calculator.lower()
-db = connect("tmp.json")
+
+oldjson = "tmp.json"
+newjson = "tmp_new.json"
+
+db1 = connect(oldjson)
+db2 = connect(newjson)
 #
 add_volume  = True
 add_entropy = True
@@ -25,26 +30,27 @@ else:
 #
 # add volume and entropy information for all the molecules in database
 #
-Nmol = len(db)
-
-newid = 1
-for imol in range(Nmol+10): # add 10 for safety
+Nmol = len(db1)
+for imol in range(Nmol): # add 10 for safety
 	try:
 		id = imol+1
-		mol = db.get(id=id).name
-		tmp = db.get_atoms(id=id)
+		mol = db1.get(id=id).name
+		tmp = db1.get_atoms(id=id)
 		print "adding volume and entropy",mol
 		magmom = tmp.get_initial_magnetic_moments()
 		#
 		# volume and molecular total entropy calculation
 		#
 		if add_volume:
+			tmp.calc = Gaussian(method=method, basis=basis)
+			BFGS(tmp).run(fmax=0.05)
 			tmp.calc = Gaussian(method=method, basis=basis, volume='tight')
 			tmp.get_potential_energy()
 			vol = tmp.get_molecular_volume()
-
 		if add_entropy:
-			tmp.calc = Gaussian(method=method, basis=basis, force=None, freq='noraman')
+			tmp.calc = Gaussian(method=method, basis=basis)
+			BFGS(tmp).run(fmax=0.05)
+			tmp.calc = Gaussian(method=method, basis=basis, volume='tight', force=None, freq='noraman')
 			tmp.get_potential_energy()
 			entropy = tmp.get_molecular_entropy()
 		#
@@ -59,17 +65,12 @@ for imol in range(Nmol+10): # add 10 for safety
 		#
 		# now write to database
 		#
-		print "writing to database"
+		print "writing to database with id =", id
 		if add_volume:
 			if add_entropy:
-				db.write(tmp, id=newid, key_value_pairs={'name' : mol, 'molecular_volume' : vol, 'molecular_entropy' : entropy})
+				db2.write(tmp, key_value_pairs={'name' : mol, 'molecular_volume' : vol, 'molecular_entropy' : entropy})
 			else:
-				db.write(tmp, id=newid, key_value_pairs={'name' : mol, 'molecular_volume' : vol})
-
-		oldid = id
-		db.delete([oldid])
-		newid += 1
-
+				db.write(tmp, key_value_pairs={'name' : mol, 'molecular_volume' : vol})
 	except:
 		print "has nothing --- go to next"
 
