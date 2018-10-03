f5e4795f50fbeb32d4f2aec6d327ae7814543a5a
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2018-10-03 18:21:46 +0900

    from ito

--- get_dos_and_charge.py
+++ get_dos_and_charge.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os, sys, re, json
+import os, sys, re, json, math
 from reaction_tools import *
 from ase import Atoms, Atom
 from ase.calculators.gaussian import Gaussian
@@ -41,33 +41,29 @@ surf.set_constraint(c)
 
 ## --- VASP ---
 if "vasp" in calculator:
-	xc          = "rpbe"
-	prec        = "normal"
-	encut       = 400.0 # 213.0 or 400.0 or 500.0
-	potim       = 0.10
-	nsw         = 100
-	nsw_neb     = 20
-	nsw_dimer   = 100
-	nelmin      = 5
-	nelm        = 40 # default:40
-	ediff       = 1.0e-5
-	ediffg      = -0.1
-	kpts_surf   = [3, 3, 1]
-	ismear_surf = 0
-	sigma_surf  = 0.10
-	vacuum      = 10.0 # for gas-phase molecules. surface vacuum is set by surf.py
-	setups      = None
-	ivdw        = 12
-	ialgo       = 48 # normal=38, veryfast=48
-	npar        = 12
-	nsim        = npar
-	lwave       = False
-	lcharg      = True
+	xc      = "rpbe"
+	prec    = "normal"
+	encut   = 400.0 # 213.0 or 400.0 or 500.0
+	potim   = 0.10
+	nsw     = 100
+	nelmin  = 5
+	nelm    = 40 # default:40
+	ediff   = 1.0e-5
+	ediffg  = -0.1
+	kpts    = [3, 3, 1]
+	ismear  = 0
+	sigma   = 0.10
+	vacuum  = 10.0 # for gas-phase molecules. surface vacuum is set by surf.py
+	setups  = None
+	ivdw    = 12
+	ialgo   = 48 # normal=38, veryfast=48
+	npar    = 12
+	nsim    = npar
+	lwave   = False
 	#setups = {"O" : "_h"}
 
 	method = xc
 	basis  = ""
-	label  = method
 
 	# DFT+U
 	DFTU = False
@@ -79,37 +75,39 @@ if "vasp" in calculator:
 	# charge
 	neutral = True
 
-## --- EMT --- -> nothing to set
-elif "emt" in calculator:
-	method = ""
-	basis  = ""
-	label  = ""
-
+label  = surf.get_chemical_formula()
+magmom = surf.get_initial_magnetic_moments()
+# spin switch
+if int( math.ceil(sum(magmom)) )!=0:
+	print "has unpaired electron"
+	ispin = 2
+else:
+	ispin = 1
 #
 # surface
 #
 if neutral:
 	if DFTU:
-		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=2, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
-						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=lcharg,
+		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=ispin, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
+						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=False,
 						ibrion=2, potim=potim, nsw=nsw, ediff=ediff, ediffg=ediffg, kpts=kpts, 
 						ldau=ldau, ldautype=ldautype, ldau_luj=ldau_luj ) # DFT+U
 	else:
- 		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=2, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
-						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=lcharg,
+ 		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=ispin, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
+						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=False,
 						ibrion=2, potim=potim, nsw=nsw, ediff=ediff, ediffg=ediffg, kpts=kpts) # normal
 else:
 	nelect = get_number_of_valence_electrons(surf)
 	nelect = nelect - charge
 
 	if DFTU:
- 		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=2, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
-						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=lcharg,
+ 		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=ispin, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
+						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=False,
 						ibrion=2, potim=potim, nsw=nsw, ediff=ediff, ediffg=ediffg, kpts=kpts, 
 						nelect=nelect, lmono="true", ldau=ldau, ldautype=ldautype, ldau_luj=ldau_luj ) # charge + DFT+U
 	else:
- 		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=2, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
-						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=lcharg,
+ 		surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=ispin, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
+						encut=encut, ismear=ismear, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave, lcharg=False,
 						ibrion=2, potim=potim, nsw=nsw, ediff=ediff, ediffg=ediffg, kpts=kpts, 
 						nelect=nelect, lmono="true") # charge
 
@@ -117,12 +115,11 @@ else:
 surf.get_potential_energy()
 
 # single point
+prec_sp   = "accurate"
 ismear_sp = -5
 kpts_sp   = [5, 5, 1]
 
-label = surf.formula()
-
-surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=2, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
+surf.calc = Vasp(label=label, prec=prec_sp, xc=xc, ispin=ispin, nelm=nelm, nelmin=nelmin, ivdw=ivdw, npar=npar, nsim=nsim,
 				encut=encut, ismear=ismear_sp, istart=0, setups=setups, sigma=sigma, ialgo=ialgo, lwave=lwave,
 				ibrion=-1, potim=potim, nsw=0, ediff=ediff, ediffg=ediffg, kpts=kpts_sp,
 				lcharg=True, laechg=True) # normal
@@ -130,4 +127,11 @@ surf.calc = Vasp(label=label, prec=prec, xc=xc, ispin=2, nelm=nelm, nelmin=nelmi
 surf.get_potential_energy()
 
 os.system('rm PCDAT XDATCAR EIGENVAL OSZICAR IBZKPT CHG WAVECAR REPORT >& /dev/null')
+
+home = os.environ['HOME']
+chgsum = os.path.join(home,"vasp/vtstscripts/vtstscripts-935/chgsum.pl")
+bader  = os.path.join(home,"vasp/bader/bader")
+
+os.system('%s %s/AECCAR0 %s/AECCAR2'     % (chgsum, label, label) )
+os.system('%s %s/CHGCAR -ref CHGCAR_sum' % (bader,  label) )
 
