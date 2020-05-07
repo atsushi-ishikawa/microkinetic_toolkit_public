f5d7176480d86f440d6f2d117f9ebeacdc15c543
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2020-05-07 15:38:02 +0900

    from ito

--- submit.py
+++ submit.py
@@ -1,10 +1,22 @@
 import os,sys
 from reaction_tools import get_number_of_reaction
 
-each = 5
-inp = "ads.txt"
+# usage: submit.py inputfile [number of jobs]
+
+submit_sh = "run_kyushu.sh"
+argvs = sys.argv
+inp   = argvs[1]
 rxn_num = get_number_of_reaction(inp)
-print("rxn_num = %d" % rxn_num)
+
+if len(argvs)==3:
+	# -- specify number of jobs
+	num_jobs = int(argvs[2])
+	each = rxn_num // num_jobs
+else:
+	# --  specify reactions per jobs
+	each = 5
+
+print("rxn_num = %d, rxns per jobs = %d" % (rxn_num, each))
 
 # remove old files
 os.system("rm -rf pbe*")
@@ -14,15 +26,15 @@ os.system("rm std*")
 
 st = 0
 ed = 1
-last = False
 
+last = False
 while True:
 	ed = st + each
 	if ed >= rxn_num:
 		ed = rxn_num
 		last = True
 
-	command = "pjsub -x \"INP={0}\" -x \"ST={1}\" -x \"ED={2}\" run_rxn.sh".format(inp,st,ed)
+	command = "pjsub -x \"INP={0}\" -x \"ST={1}\" -x \"ED={2}\" {3}".format(inp,st,ed,submit_sh)
 	print(command)
 	os.system(command)
 
