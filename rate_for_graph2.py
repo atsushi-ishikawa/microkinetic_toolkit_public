c4d83c03c2309d19998e174f142dce51730ce69f
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2021-08-30 16:58:23 +0900

    from mac

--- rate_for_graph2.py
+++ rate_for_graph2.py
@@ -1,13 +1,13 @@
 import os, sys
 from reaction_tools import *
- 
-argvs = sys.argv
-reactionfile  = argvs[1]
-coveragefile  = argvs[2]  # time-dependent coverage file from MATLAB. Saved as "nodes.txt" in MATLAB
-rateconstfile = argvs[3]  # rate constants from MATLAB
-variablefile  = argvs[4]  # sden, area, Vr
 
-(r_ads, r_site, r_coef,  p_ads, p_site, p_coef) = get_reac_and_prod(reactionfile)
+argvs = sys.argv
+reactionfile = argvs[1]
+coveragefile = argvs[2]  # time-dependent coverage file from MATLAB. Saved as "nodes.txt" in MATLAB
+rateconstfile = argvs[3]  # rate constants from MATLAB
+variablefile = argvs[4]  # sden, area, Vr
+
+(r_ads, r_site, r_coef, p_ads, p_site, p_coef) = get_reac_and_prod(reactionfile)
 rxn_num = get_number_of_reaction(reactionfile)
 spe_num = get_species_num()
 
@@ -22,7 +22,7 @@ Nrxn = 0
 for iline, line in enumerate(lines):
 	name = str(line.split()[0])
 	if "R" not in name:
-		spe  = int(line.split()[1])-1  # Matlab --> python
+		spe = int(line.split()[1]) - 1  # Matlab --> python
 		conc = float(line.split()[2])
 		time = float(line.split()[3])
 
@@ -30,7 +30,7 @@ for iline, line in enumerate(lines):
 	else:
 		Nrxn += 1
 
-maxtime = (len(lines)-Nrxn)//spe_num
+maxtime = (len(lines) - Nrxn) // spe_num
 
 #
 # read rate constants
@@ -47,7 +47,7 @@ f.close()
 f = open(variablefile, 'r')
 sden = float(f.readline().replace("\n", ""))
 area = float(f.readline().replace("\n", ""))
-Vr   = float(f.readline().replace("\n", ""))
+Vr = float(f.readline().replace("\n", ""))
 f.close()
 
 for iline, line in enumerate(lines):
@@ -57,7 +57,7 @@ for iline, line in enumerate(lines):
 
 rate = [0 for i in range(rxn_num)]
 
-AoverV = area/Vr
+AoverV = area / Vr
 
 edgefile = 'edges.txt'
 f1 = open(edgefile, 'w')
@@ -70,10 +70,10 @@ for istep in range(maxtime):
 		#
 		val = kfor[irxn]
 		conc_all = 1.0
-		for imol,mol in enumerate(r_ads[irxn]):
+		for imol, mol in enumerate(r_ads[irxn]):
 			mol = mol[0]
 			mol = remove_side_and_flip(mol)
-		
+
 			site = r_site[irxn][imol]
 			if 'gas' not in site:
 				# surface species
@@ -86,15 +86,15 @@ for istep in range(maxtime):
 				name, conc, time = cov[spe][istep]
 
 			power = r_coef[irxn][imol]
-			if power != 1:
-				conc = conc**power
+			if power!=1:
+				conc = conc ** power
 			conc_all = conc_all * conc
 
-		for_rate = val*conc_all
+		for_rate = val * conc_all
 		#
 		# now we write to file
 		#
-		now = 'R%03d' % irxn # current reaction
+		now = 'R%03d' % irxn  # current reaction
 
 		for imol, mol in enumerate(r_ads[irxn]):
 			mol = remove_side_and_flip(mol[0])
@@ -106,7 +106,7 @@ for istep in range(maxtime):
 			name, conc, time = cov[spe][istep]
 
 			f1.write("%8s %8s %12.4e %12.4e\n" % (name, now, for_rate, time))
-		
+
 		#
 		# product side
 		#
@@ -123,7 +123,7 @@ for istep in range(maxtime):
 
 f1.close()
 
-#f = open(coveragefile, 'a')
-#for i in range(rxn_num):
+# f = open(coveragefile, 'a')
+# for i in range(rxn_num):
 #	f.write("          RXN%03d%5.d%14.5e%14.5f\n" % (i, i+100, 1.0, 0.0))
-#f.close()
+# f.close()
