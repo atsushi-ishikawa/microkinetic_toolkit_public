ca1803159a20dff3e821099dd74a7208ac8c11e2
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2021-11-08 17:56:13 +0900

    restructured

--- microkinetic_toolkit/test.py
+++
@@ -1,9 +0,0 @@
-from reactions import Reaction, Reactions
-from tinydb import TinyDB
-import os
-import glob
-for file in glob.glob("test.json"):
-	os.remove(file)
-
-rxns = Reactions.from_csv("test.csv")
-print(rxns[1]._reaction_str)
