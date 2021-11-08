4c2b74dfe3d925189ba32860a2d4245245bd3906
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2021-11-08 18:04:50 +0900

    made document

--- microkinetic_toolkit/tmpode.py
+++ microkinetic_toolkit/tmpode.py
@@ -8,13 +8,13 @@ def func(t, c, kfor, Kc, T, sden, area, Vr, ngas, ncomp):
 	theta = theta * sden
 	rate = np.zeros(5)
 
-	rate[0] = - 1.0*kfor[1]*c[1]*c[1] - 1.0*kfor[1]*c[1]*c[1]*c[1]  # O2
-	rate[1] = - 1.0*kfor[1]*c[1]*c[1]*c[1]*c[1]*c[1] - 1.0*kfor[1]*c[1]*c[1]*c[1]*c[1]*c[1]*c[1]  # CO2
-	rate[2] = - 1.0*kfor[0]*c[2]*c[2]*c[2]*c[2]*c[2] - 1.0*kfor[0]*c[2]*c[2]*c[2]*c[2]*c[2]*c[2]  # H
-	rate[3] = - 1.0*kfor[0]*c[2]*c[2] - 1.0*kfor[0]*c[2]*c[2]*c[2] - 1.0*kfor[1]*c[1]*c[1] - 1.0*kfor[1]*c[1]*c[1]*c[1]  # CO
-	rate[4] = - 1.0*kfor[0]*c[2]*c[2] - 1.0*kfor[0]*c[2]*c[2]*c[2]  # H2
+	rate[0] = - 1.0*kfor[1]*c[0]*c[0]*c[0]*c[0]*c[0] - 1.0*kfor[1]*c[0]*c[0]*c[0]*c[0]*c[0]*c[0]  # CO2
+	rate[1] = - 1.0*kfor[0]*c[1]*c[1]*c[1]*c[1]*c[1] - 1.0*kfor[0]*c[1]*c[1]*c[1]*c[1]*c[1]*c[1]  # H
+	rate[2] = - 1.0*kfor[1]*c[0]*c[0] - 1.0*kfor[1]*c[0]*c[0]*c[0]  # O2
+	rate[3] = - 1.0*kfor[0]*c[1]*c[1] - 1.0*kfor[0]*c[1]*c[1]*c[1] - 1.0*kfor[1]*c[0]*c[0] - 1.0*kfor[1]*c[0]*c[0]*c[0]  # CO
+	rate[4] = - 1.0*kfor[0]*c[1]*c[1] - 1.0*kfor[0]*c[1]*c[1]*c[1]  # H2
 
-	# species --- 0 = O2 1 = CO2 2 = H 3 = CO 4 = H2 
+	# species --- 0 = CO2 1 = H 2 = O2 3 = CO 4 = H2 
 	if ncomp > ngas:
 		rate[ngas:ncomp] = rate[ngas:ncomp]*(1/sden)  # surface
 	return rate
