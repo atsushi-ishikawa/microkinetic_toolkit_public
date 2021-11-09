7531294d8c93c505814d34fd1c255f2c16d7df3b
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2021-11-09 18:39:56 +0900

    made setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="microkinetic_toolkit",
+	version="0.0.0",
+	description="package for microkinetic analysis",
+
+	author="Atsushi Ishikawa",
+	author_email="ishikawa.atsushi@nims.go.jp",
+
+	packages=[
+		"microkinetic_toolkit"
+	]
+)
