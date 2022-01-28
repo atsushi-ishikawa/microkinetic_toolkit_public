09dee4a3fc43f3cbc83768d5d0f6b9171ffbf40f
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2022-01-28 18:54:15 +0900

    removed some unnecessary files

--- example.py
+++ example.py
@@ -12,44 +12,46 @@ ratio = {"H2": 1.1, "CO2": 1.0}  # partial pressure ratio
 # whether save files or not
 deltaEs_pickle = "deltaEs.pickle"
 
-## read reactions from file and set Reactions
+# read reactions from file and set Reactions
 reactions = Reactions.from_csv("test.csv")
 
-## define surface if you like
+# define surface if you like
 reactions.surface = fcc111("Ni", size=[3, 3, 4], vacuum=10.0, periodic=True)
 
 I_have_precalculated_database = False
 if I_have_precalculated_database:
-	reactions.ase_db = "ase.db"
+    reactions.ase_db = "ase.db"
 
 # reaction energy evaluation
 reactions.calculator = "emt"
 if os.path.exists(deltaEs_pickle):
-	deltaEs = pickle.load(open(deltaEs_pickle, "rb"))
+    deltaEs = pickle.load(open(deltaEs_pickle, "rb"))
 else:
-	deltaEs = reactions.get_reaction_energies()
-	with open(deltaEs_pickle, "wb") as f:
-		pickle.dump(deltaEs, f)
+    deltaEs = reactions.get_reaction_energies()
+    with open(deltaEs_pickle, "wb") as f:
+        pickle.dump(deltaEs, f)
 
 # microkinetic analysis
-## set parameters for kinetics
-bep_param = {"alpha": 0.6, "beta": 1.0}  # BEP relatinoship (Ea = alpha*deltaE + beta)
+# set parameters for kinetics
+# BEP relatinoship (Ea = alpha*deltaE + beta)
+bep_param = {"alpha": 0.6, "beta": 1.0}
 sden = 1.0e-4  # site density
 v0 = 1.0e-3  # volumetric flowrate [m^3/sec]
 wcat = 100.0e-3  # catalyst weight [mg]
 phi = 0.5  # porocity
 rho_b = 1.0e3  # density
 
-reactions.set_kinetic_parameters(bep_param=bep_param, sden=sden, v0=v0, wcat=wcat, phi=phi, rho_b=rho_b)
+reactions.set_kinetic_parameters(bep_param=bep_param, sden=sden,
+                                 v0=v0, wcat=wcat, phi=phi, rho_b=rho_b)
 
-## calculate rate constant from reaction energies
+# calculate rate constant from reaction energies
 kfor = reactions.get_rate_constants(deltaEs=deltaEs, T=T)
 
-## do_microkinetics
+# do_microkinetics
 reactions.do_microkinetics(deltaEs=deltaEs, kfor=kfor, T=T, P=P, ratio=ratio)
 reactions.get_rate_for_graph(T=T)
 
-## draw graph
+# draw graph
 graph = ReactionsVisualizer(reactions)
 graph.draw_network()
 
