4e810c5c97995ae538286f15825969ffc5a15adf
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2022-02-22 13:25:12 +0900

    copied from develop

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import os
 import pickle
 
 # parameters
-T = 800  # temperature [K]
+T = 900  # temperature [K]
 P = 100  # total pressure [bar]
 ratio = {"H2": 1.1, "CO2": 1.0}  # partial pressure ratio
 
@@ -34,24 +34,24 @@ else:
 # microkinetic analysis
 # set parameters for kinetics
 # BEP relatinoship (Ea = alpha*deltaE + beta)
-bep_param = {"alpha": 0.6, "beta": 1.0}
-sden = 1.0e-4  # site density
-v0 = 1.0e-3  # volumetric flowrate [m^3/sec]
-wcat = 100.0e-3  # catalyst weight [mg]
-phi = 0.5  # porocity
-rho_b = 1.0e3  # density
+bep_param = {"alpha": 0.84, "beta": 1.6}
+sden  = 1.0e-5    # site density [mol/m^2]
+v0    = 1.0e-6    # volumetric flowrate [m^3/sec]; 1.0e-6 [m^3/sec] = 1.0 [mL/sec]
+area  = 1.0e3     # surface area (e.g. BET) [m^2/kg]. Typical value is 1.0e3.
+wcat  = 1.0e-3    # catalyst weight [kg]
+phi   = 0.5       # porocity
+rho_b = 1.0e3     # density [kg/m^3]
 
-reactions.set_kinetic_parameters(bep_param=bep_param, sden=sden,
-                                 v0=v0, wcat=wcat, phi=phi, rho_b=rho_b)
+reactions.set_kinetic_parameters(bep_param=bep_param, sden=sden, v0=v0, wcat=wcat, area=area, phi=phi, rho_b=rho_b)
 
 # calculate rate constant from reaction energies
 kfor = reactions.get_rate_constants(deltaEs=deltaEs, T=T)
 
 # do_microkinetics
 reactions.do_microkinetics(deltaEs=deltaEs, kfor=kfor, T=T, P=P, ratio=ratio)
-reactions.get_rate_for_graph(T=T)
+#sens = reactions.get_sensitivity(deltaEs=deltaEs, kfor=kfor, T=T, P=P, ratio=ratio)
+#reactions.get_rate_for_graph(T=T)
 
 # draw graph
-graph = ReactionsVisualizer(reactions)
-graph.draw_network()
-
+#graph = ReactionsVisualizer(reactions)
+#graph.draw_network()
